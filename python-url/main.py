6d633b69a8ba1ed3237f9932a879f50d2afc0bf7
Roni Wellington <roniwellington@gmail.com>
2023-03-22 10:07:38 -0300

    estudo python

--- python-url/main.py
+++ python-url/main.py
@@ -1,1 +1,15 @@
-url = "http://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
+url = "https://bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
+print(url)
+
+indice_interrogacao = url.find('?')
+url_base = url[0:indice_interrogacao]
+
+url_parametros = url[indice_interrogacao+1:]
+print(url_parametros)
+
+parametro_busca = 'moedaDestino'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+valor = url_parametros[indice_valor:]
+
+print(valor)
