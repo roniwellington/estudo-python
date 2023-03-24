bfd0a7ac0c18033e348170ce0ffe782fe7311e9a
Roni Wellington <roniwellington@gmail.com>
2023-03-24 16:35:24 -0300

    estudo de python

--- python-url/main.py
+++ python-url/main.py
@@ -1,15 +1,29 @@
-url = "https://bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
-print(url)
+#url = "https://bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
+url="  "
 
+#Sanitização da URL
+url = url.replace(" ","")
+
+#Validação da URL
+if url == "":
+    raise ValueError("A URL está vazia")
+
+
+#Separa base e s parâmetros
 indice_interrogacao = url.find('?')
-url_base = url[0:indice_interrogacao]
+url_base = url[:indice_interrogacao]
 
 url_parametros = url[indice_interrogacao+1:]
 print(url_parametros)
 
-parametro_busca = 'moedaDestino'
+#Busca o valor de um parâmetro
+parametro_busca = 'quantidade'
 indice_parametro = url_parametros.find(parametro_busca)
 indice_valor = indice_parametro + len(parametro_busca) + 1
-valor = url_parametros[indice_valor:]
-
+indice_e_comercial = url_parametros.find('&',indice_valor)
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
 print(valor)
+
